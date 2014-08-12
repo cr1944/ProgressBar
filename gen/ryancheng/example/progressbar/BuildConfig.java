2858cc5448f9ebcf56e11504b5ee94a32240f176
ryancheng <ryancheng1944@gmail.com>
2014-08-12 17:37:56 +0800

    first commit

---
+++ gen/ryancheng/example/progressbar/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ryancheng.example.progressbar;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/ryancheng/example/progressbar/MainActivity.java
@@ -0,0 +1,16 @@
+
+package ryancheng.example.progressbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
